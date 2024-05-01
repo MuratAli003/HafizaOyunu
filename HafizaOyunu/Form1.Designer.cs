f8b76663f8e3b20e5a694bca386a4d1937d15fe9
huawei <huawei@LAPTOP-2R5A6JR2>
2024-05-02 01:00:11 +0300

    Proje dosyası ekle.

---
+++ HafizaOyunu/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace HafizaOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.bilgi = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button2.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button2.Location = new System.Drawing.Point(12, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 94);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button3.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(256, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 94);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button4.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button4.Location = new System.Drawing.Point(12, 106);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 94);
+            this.button4.TabIndex = 5;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button5.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button5.Location = new System.Drawing.Point(134, 199);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(122, 94);
+            this.button5.TabIndex = 7;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button6.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button6.Location = new System.Drawing.Point(378, 16);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(122, 94);
+            this.button6.TabIndex = 9;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button7.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button7.Location = new System.Drawing.Point(378, 106);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(122, 94);
+            this.button7.TabIndex = 11;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button8.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button8.Location = new System.Drawing.Point(134, 16);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(122, 94);
+            this.button8.TabIndex = 13;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button9.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button9.Location = new System.Drawing.Point(12, 292);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(122, 94);
+            this.button9.TabIndex = 15;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button10.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button10.Location = new System.Drawing.Point(256, 16);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(122, 94);
+            this.button10.TabIndex = 17;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button11.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button11.Location = new System.Drawing.Point(12, 199);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(122, 94);
+            this.button11.TabIndex = 19;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button12.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button12.Location = new System.Drawing.Point(256, 199);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(122, 94);
+            this.button12.TabIndex = 21;
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button13.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button13.Location = new System.Drawing.Point(378, 199);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(122, 94);
+            this.button13.TabIndex = 21;
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button14.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button14.Location = new System.Drawing.Point(134, 106);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(122, 94);
+            this.button14.TabIndex = 23;
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button15.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button15.Location = new System.Drawing.Point(134, 292);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(122, 94);
+            this.button15.TabIndex = 25;
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button16.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button16.Location = new System.Drawing.Point(256, 292);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(122, 94);
+            this.button16.TabIndex = 27;
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.button17.Font = new System.Drawing.Font("Webdings", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button17.Location = new System.Drawing.Point(378, 292);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(122, 94);
+            this.button17.TabIndex = 29;
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // bilgi
+            // 
+            this.bilgi.AutoSize = true;
+            this.bilgi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bilgi.Location = new System.Drawing.Point(12, 412);
+            this.bilgi.Name = "bilgi";
+            this.bilgi.Size = new System.Drawing.Size(0, 25);
+            this.bilgi.TabIndex = 30;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(445, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 29);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "\r\n";
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(312, 412);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 32;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(512, 455);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bilgi);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HafızaOyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label bilgi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HafizaOyunu/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+namespace HafizaOyunu
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();// Iconlari rastgele dagitmak icin olusturuldu 
+        List<string> icons = new List<string>()
+        {
+            "-", "-", "N", "N", ".", ".", "K", "K",
+            "%", "%", "V", "V", "w", "w", "s", "s"
+        };// İcon olusturulmak icin olusturuldu
+
+        Button first, second;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Ekran acildiginda kartlari 4 saniye gösterir.
+            timer1.Start();
+            bilgi.Text = "Oyun Basliyor...";
+            timer1.Interval = 4000;
+            
+        }
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Button btn;
+
+            //Butonlari okuma islemi
+            foreach(Button x in Controls.OfType<Button>())
+            {
+                btn = x as Button;
+
+                //Butona icon atama
+                int rndIndex = rnd.Next(icons.Count);
+
+                btn.Text = icons[rndIndex];
+
+                //Buton renkleri olusturuldu
+                btn.ForeColor = Color.Black;
+
+                btn.BackColor = Color.White;
+
+                //Tekrara dusmemek icin listede kullanılan eleman silindi
+                icons.RemoveAt(rndIndex);
+            }   
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer3.Start();
+            label2.Text = "Kalan Sure:";
+            
+
+            bilgi.Text = "Ayni iki karti bul!!";
+
+            //Baslangıcta kartlar gosterildikten sonra butun kartlari kapatma islemi
+            foreach(Button x in Controls.OfType<Button>())
+            {
+                x.ForeColor = Color.Gray;
+                x.BackColor = x.ForeColor;
+            }
+   
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            bilgi.Text = "Tekrar Dene!!";
+
+            //Yanlis tahmin yapildigi icin kartlar kapatilir
+            first.ForeColor = second.ForeColor = Color.Gray;
+            first.BackColor = second.BackColor = Color.Gray;
+            first = null;
+            second = null;
+
+        }
+
+        double sure = 900.0;//Oyun süresi
+        
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            double kalanSure = (sure * 0.1);
+            if (sure > 0)
+            {
+                sure--;
+                label1.Text = Convert.ToInt32(kalanSure).ToString();
+            }
+            else
+            {
+                timer3.Stop();
+                MessageBox.Show("Sure Doldu.Kaybettiniz!!");
+                
+                Close();
+            }
+        }
+
+        int sayac = 0; // Butun tahminler bittikten sonra oyun bitirmek icin olusturuldu
+        private void button17_Click(object sender, EventArgs e)
+        {
+            //Tiklanan butonu okur
+            Button button = sender as Button;
+
+            if(first == null)
+            {
+                //Ilk kart acilir
+                first = button;
+                first.ForeColor = Color.Black;
+                first.BackColor = Color.White;
+                return;
+
+            }
+            //Ikinci karti acar
+            second = button;
+            second.ForeColor = Color.Black;
+            second.BackColor = Color.White;
+
+            //Kartlarin esitligi sorgulanir
+            if(first.Text == second.Text)
+            {
+                first = null;
+                second = null;
+
+                bilgi.Text = "Dogru!!";
+                sayac++;
+                //Toplam tahmin sayisi 8 dir.
+                if(sayac == 8)
+                {
+                    timer3.Stop();
+                    bilgi.Text = "Oyun Bitti!!";
+                    MessageBox.Show("Tebrikler Oyunu Kazandiniz");
+                    Close();
+                }               
+            }
+            else
+            {
+                //Yanlis tahmin durumunda kartlari kapatmak icin kullanilir.
+                timer2.Start();
+                timer2.Interval = 400;
+
+            }
+
+        }
+    }  
+}
+
